2302bf3eaa7797bbc976b3d3ed933eeb4a21cc72
yebin <yebind3308@gmail.com>
2025-01-15 22:44:04 +0900

    feat: Contents > Project Component Settings

--- src/components/Contents/Project/index.tsx
+++ src/components/Contents/Project/index.tsx
@@ -1,21 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import "./index.css";
 import { PROJECT_LIST } from "../../../constants/projectList";
+import { Icon } from "@iconify/react";
 
 const ProjectComponent = () => {
+  const [active, setActive] = useState(false);
+
   return (
-    <article className="project">
-      <section className="projectGrid">
+    <section className="project">
+      <ul className="projectGrid">
         {PROJECT_LIST.map((item) => (
-          <div className="projectItem" key={item.title}>
+          <li
+            className={`projectItem ${active ? "active" : ""}`}
+            key={item.title}
+          >
             <div className="imageWrap">
-              <img src={item.image} alt={item.title} />
+              <img
+                src={item.image}
+                alt={item.title}
+                onLoad={() => setActive(true)}
+              />
             </div>
-            <p>{item.title}</p>
-          </div>
+
+            <div className="descriptionWrap">
+              <p>{item.title}</p>
+              <button
+                type="button"
+                onClick={() => window.open(item.link, "_blank")}
+              >
+                Git Hub
+              </button>
+            </div>
+            <div className="iconWrap">
+              {item.icon.map((icons, index) => (
+                <Icon
+                  className="icon"
+                  key={index}
+                  icon={icons}
+                  style={{ transform: `translateX(${-2 + index * -5}px)` }}
+                />
+              ))}
+            </div>
+          </li>
         ))}
-      </section>
-    </article>
+      </ul>
+    </section>
   );
 };
 
--- src/constants/projectList.ts
+++ src/constants/projectList.ts
@@ -7,33 +7,59 @@ import Portfolio from "../assets/images/Portfolio.png";
 
 export const PROJECT_LIST = [
   {
-    title: "인테리어 추천 커뮤니티",
-    image: InteriorCommunity,
-    link: "https://github.com/YebinYun/seb45-main-027",
+    title: "개인 포트폴리오",
+    image: Portfolio,
+    link: "https://github.com/YebinYun/portfolio-dev",
+    icon: ["lineicons:typescript", "lineicons:react", "akar-icons:css-fill"],
   },
   {
     title: "전세계 호텔 조회 사이트 ",
     image: HotelSite,
     link: "https://github.com/YebinYun/hotel-reservation-client",
+    icon: [
+      "lineicons:nextjs",
+      "lineicons:react",
+      "lineicons:nodejs-alt",
+      "devicon-plain:mongodb-wordmark",
+      "simple-icons:reactquery",
+      "simple-icons:recoil",
+      "simple-icons:mui",
+    ],
+  },
+  {
+    title: "인테리어 추천 커뮤니티",
+    image: InteriorCommunity,
+    link: "https://github.com/YebinYun/seb45-main-027",
+    icon: ["lineicons:react", "lineicons:javascript", "mdi:tailwind"],
   },
   {
     title: "음식 추천 룰렛",
     image: Roulette,
     link: "https://github.com/YebinYun/lunch-recommendation",
+    icon: [
+      "lineicons:typescript",
+      "lineicons:nodejs-alt",
+      "lineicons:react",
+      "lineicons:javascript",
+      "simple-icons:styledcomponents",
+    ],
   },
   {
     title: "음식 추천 설문지",
     image: Questionnaire,
     link: "https://github.com/YebinYun/what-eat-today",
+    icon: ["lineicons:react", "lineicons:javascript", "hugeicons:bootstrap"],
   },
   {
     title: "운동 기록 트래커",
     image: ExerciseRecordTracker,
     link: "https://github.com/YebinYun/exercise-tracker-web",
-  },
-  {
-    title: "개인 포트폴리오",
-    image: Portfolio,
-    link: "https://github.com/YebinYun/portfolio-dev",
+    icon: [
+      "lineicons:react",
+      "lineicons:javascript",
+      "mdi:tailwind",
+      "simple-icons:styledcomponents",
+      "bxl:redux",
+    ],
   },
 ];
