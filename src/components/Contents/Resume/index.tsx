6518317f91cc85a3fbacfab6e72d55f493e5f6e3
yebin <yebind3308@gmail.com>
2025-01-20 23:36:15 +0900

    fix: Modifying a Resume document-1

--- src/components/Contents/Resume/index.tsx
+++ src/components/Contents/Resume/index.tsx
@@ -3,15 +3,28 @@ import "react-pdf/dist/Page/TextLayer.css";
 import "react-pdf/dist/Page/AnnotationLayer.css";
 import { pdfjs, Document, Page } from "react-pdf";
 import PdfFile from "../../../assets/document/resume.pdf";
+import { useState } from "react";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
 const ResumeComponent = () => {
+  const [numPages, setNumPages] = useState<number>(0);
+
+  const onDocumentLoadSuccess = ({ numPages }: pdfjs.PDFDocumentProxy) => {
+    setNumPages(numPages);
+  };
+
   return (
     <div className="resume">
       <button onClick={() => window.open(PdfFile, "_blank")}>Download</button>
-      <Document file={PdfFile}>
-        <Page className="pdf-page" pageNumber={1} />
+      <Document file={PdfFile} onLoadSuccess={onDocumentLoadSuccess}>
+        {Array.from(new Array(numPages), (_, index) => (
+          <Page
+            className="pdf-page"
+            key={`page_${index + 1}`}
+            pageNumber={index + 1}
+          />
+        ))}
       </Document>
     </div>
   );
