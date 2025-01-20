d498b28c7d6734d7ed04c68c5d72d9581af36c23
yebin <yebind3308@gmail.com>
2025-01-20 22:05:05 +0900

    fix: Modifying a Resume document

--- src/components/Contents/Resume/index.tsx
+++ src/components/Contents/Resume/index.tsx
@@ -2,20 +2,16 @@ import "./index.css";
 import "react-pdf/dist/Page/TextLayer.css";
 import "react-pdf/dist/Page/AnnotationLayer.css";
 import { pdfjs, Document, Page } from "react-pdf";
-import PdfFile from "./resume.pdf";
+import PdfFile from "../../../assets/document/resume.pdf";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
 const ResumeComponent = () => {
-  const pages = [1, 2];
-
   return (
     <div className="resume">
       <button onClick={() => window.open(PdfFile, "_blank")}>Download</button>
       <Document file={PdfFile}>
-        {pages.map((page) => (
-          <Page className="pdf-page" key={page} pageNumber={page} />
-        ))}
+        <Page className="pdf-page" pageNumber={1} />
       </Document>
     </div>
   );
